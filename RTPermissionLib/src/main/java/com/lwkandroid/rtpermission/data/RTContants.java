02fc16db17b069e8db31e052a16f416fedf7b422
unknown <519097965@qq.com>
2018-06-14 08:43:17 +0800

    first commit

---
+++ RTPermissionLib/src/main/java/com/lwkandroid/rtpermission/data/RTContants.java
@@ -0,0 +1,27 @@
+package com.lwkandroid.rtpermission.data;
+
+/**
+ * 常量
+ */
+
+public class RTContants
+{
+    public static final String INTENT_KEY = "options";
+
+    /**
+     * 权限组分类
+     */
+    public static final class Permisson
+    {
+        public static final int CONTACT = 0X0;
+        public static final int PHONE = 0X1;
+        public static final int CALENDAR = 0X2;
+        public static final int CAMERA = 0X3;
+        public static final int SENSORS = 0X4;
+        public static final int LOCATION = 0X5;
+        public static final int STORAGE = 0X6;
+        public static final int MICPHONE = 0X7;
+        public static final int SMS = 0X8;
+        public static final int UNKOWN = 0X9;
+    }
+}
---
+++ RTPermissionLib/src/main/java/com/lwkandroid/rtpermission/utils/RTUtils.java
@@ -0,0 +1,81 @@
+package com.lwkandroid.rtpermission.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+
+/**
+ * 工具类
+ */
+
+public class RTUtils
+{
+    private RTUtils()
+    {
+    }
+
+    /**
+     * 获取App名称
+     */
+    public static String getAppName(Context context)
+    {
+        try
+        {
+            PackageManager pm = context.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+            return pi == null ? null : pi.applicationInfo.loadLabel(pm).toString();
+        } catch (PackageManager.NameNotFoundException e)
+        {
+            e.printStackTrace();
+            return context.getPackageName();
+        }
+    }
+
+    /**
+     * 跳转到App设置详情界面
+     */
+    public static void goToSettingDetail(Context context)
+    {
+        Intent intent = new Intent("android.settings.APPLICATION_DETAILS_SETTINGS");
+        intent.setData(Uri.parse("package:" + context.getPackageName()));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+    /**
+     * 检查是否有某项权限的试用权
+     *
+     * @param context    Context环境
+     * @param permission 权限
+     * @return true:有 false:没有
+     */
+    public static boolean hasPermission(Context context, String permission)
+    {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return true;
+
+        try
+        {
+            if (context.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED)
+            {
+                String op = AppOpsManagerCompat.permissionToOp(permission);
+                return AppOpsManagerCompat.noteProxyOp(context, op, context.getPackageName())
+                        == AppOpsManagerCompat.MODE_ALLOWED;
+            } else
+                return false;
+            //            String op = AppOpsManagerCompat.permissionToOp(permission);
+            //            int opResult = AppOpsManagerCompat.noteProxyOp(context, op, context.getPackageName());
+            //            int rtResult = context.checkSelfPermission(permission);
+            //            if (opResult == AppOpsManagerCompat.MODE_ALLOWED && rtResult == PackageManager.PERMISSION_GRANTED)
+            //                return true;
+            //            else
+            //                return false;
+        } catch (Exception e)
+        {
+            return false;
+        }
+    }
+}
